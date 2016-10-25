6b27a78c6927f14554639296bff90427e8aad77b
Hendrik Kits van Heyningen <hendrikkvh@gmail.com>
2016-10-25 02:25:45 -0400

    Created brute force method with PointSET and the beginning of KdTree, deleted the RectHV class since it is now provided by the algs4 jar, and also stopping all .class files from being tracked.

--- P5_Kd_Trees/KdTree.java
+++ P5_Kd_Trees/KdTree.java
@@ -1,83 +1,111 @@
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
 
 /**
- * KdTree Class 
- * Assignment 5
+ * KdTree Class Assignment 5
+ * 
  * @author Hendrik Kits van Heyningen
  */
-public class KdTree 
-{
+public class KdTree {
+
+	private SET<Point2D> pointSET;
 
 	/**
-	 *  Construct an empty set of points
+	 * Construct an empty set of points
 	 */
-	public KdTree()
-	{
-		
+	public KdTree() {
+		pointSET = new SET<Point2D>();
 	}
-	
+
 	/**
 	 * Is the set empty?
+	 * 
 	 * @return true if empty, false otherwise
 	 */
-	public boolean isEmpty()
-	{
-		return false;
+	public boolean isEmpty() {
+		return pointSET.isEmpty();
 	}
-	
+
 	/**
 	 * Return the number of points in the set
+	 * 
 	 * @return the number of points in the set
 	 */
-	public int size()
-	{
-		return 0;
+	public int size() {
+		return pointSET.size();
 	}
-	
+
 	/**
-	 * Add the point p to the set (if it is not already in the set) 
+	 * Add the point p to the set (if it is not already in the set)
+	 * 
 	 * @param p, the point to add
 	 */
-	public void insert(Point2D p) 
-	{
-		return;
+	public void insert(Point2D p) {
+		if (p == null)
+			throw new java.lang.NullPointerException();
+		pointSET.add(p);
 	}
-	
+
 	/**
 	 * Does the set contain the point p?
+	 * 
 	 * @param p, the point to check
 	 * @return true if it contains p, false otherwise
 	 */
-	public boolean contains(Point2D p)
-	{
-		return false;
+	public boolean contains(Point2D p) {
+		if (p == null)
+			throw new java.lang.NullPointerException();
+		return pointSET.contains(p);
 	}
-	
+
 	/**
 	 * Draw all of the points to standard draw
 	 */
-	public void draw()
-	{
+	public void draw() {
+		for (Point2D p : pointSET) {
+			StdDraw.point(p.x(), p.y());
+		}
 		
 	}
-	
+
 	/**
 	 * Returns all points in the set that are inside the rectangle
+	 * 
 	 * @param rect, the range to check
 	 * @return an iterator of points in the range
 	 */
-	public Iterable<Point2D> range(RectHV rect) 
-	{
-		return null;
+	public Iterable<Point2D> range(RectHV rect) {
+		if (rect == null)
+			throw new java.lang.NullPointerException();
+		SET<Point2D> pointsInRect = new SET<Point2D>();
+		for (Point2D p : this.pointSET) {
+			if (rect.contains(p))
+				pointsInRect.add(p);
+		}
+		return pointsInRect;
 	}
 
 	/**
 	 * Finds a nearest neighbor in the set to p; null if set is empty
+	 * 
 	 * @param p the point to check
 	 * @return the neighbor; null if set is empty
 	 */
-	public Point2D nearest(Point2D p)
-	{
-		return null;
+	public Point2D nearest(Point2D p) {
+		if (p == null)
+			throw new java.lang.NullPointerException();
+		Point2D closestPoint = null;
+		for (Point2D q : this.pointSET) {
+			if (closestPoint == null) {
+				closestPoint = q;
+				continue;
+			}
+			if (p.distanceTo(q) < p.distanceTo(closestPoint)) {
+				closestPoint = p;
+			}
+		}
+		return closestPoint;
 	}
 }
